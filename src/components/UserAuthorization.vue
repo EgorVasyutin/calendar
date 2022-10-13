6d3127d1b39bda870c69021cd584595de93069b3
Egor <vaspovas@gmail.com>
2022-10-13 23:28:15 +0300

    first commit

---
+++ src/components/UserAuthorization.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <AppInput placeholder="Name" label="Your name"></AppInput>
+    <AppInput placeholder="Email" label="Your name"></AppInput>
+    <AppInput placeholder="Password" label="Your Password"></AppInput>
+    <AppInput
+      placeholder="Confirm password"
+      label="Confirm password"
+    ></AppInput>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
